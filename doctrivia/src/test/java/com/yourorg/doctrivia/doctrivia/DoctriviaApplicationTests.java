e73129ca016fa964852a201532def6647180be01
nadav770 <nadavm.7703@gmail.com>
2025-06-30 21:06:21 +0300

    with spring-boot and depenc

---
+++ doctrivia/src/test/java/com/yourorg/doctrivia/doctrivia/DoctriviaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yourorg.doctrivia.doctrivia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoctriviaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
