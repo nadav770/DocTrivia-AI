736ab73eaaa4ed04e38842d3dedf718391e0f9c6
nadav770 <nadavm.7703@gmail.com>
2025-06-30 21:02:05 +0300

    with spring-boot and depenc

---
+++ doctrivia/src/main/java/com/yourorg/doctrivia/DoctriviaApplication.java
@@ -0,0 +1,13 @@
+package com.yourorg.doctrivia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoctriviaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoctriviaApplication.class, args);
+	}
+
+}
